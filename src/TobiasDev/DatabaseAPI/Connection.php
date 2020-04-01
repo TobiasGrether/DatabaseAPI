b1d72d02d12ee89e8f5241af7b751b7d7bfe5742
TobiasDev <tobiasdevde@gmail.com>
2020-04-02 00:37:57 +0200

    Upload

--- src/TobiasDev/DatabaseAPI/Connection.php
+++ src/TobiasDev/DatabaseAPI/Connection.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * Copyright (c) 2019.
+ * This Software may not be shared without explicit permission from the Developers of this System.
+ * You are not allowed to redistribute this under any terms or conditions without explicit permission.
+ * #     #   #     #  #  #    #     #   #      #      # # # # #
+ * #     # #     # #    #   #     #   #      #      #     # #     #
+ *
+ */
 
-	 namespace TobiasDev\DatabaseAPI;
+namespace TobiasDev\DatabaseAPI;
 
+	 use Closure;
 	 use pocketmine\Server;
 	 use TobiasDev\DatabaseAPI\Task\InsertionTask;
 	 use TobiasDev\DatabaseAPI\Task\SelectorTask;
@@ -28,29 +37,30 @@
 		  /**
 			* @param String        $query
 			* @param String        $database
-			* @param \Closure|null $datahandler
-			* @param \Closure|null $action
+			* @param Closure|null $datahandler
+			* @param Closure|null $action
 			* @param array         $data
+			* @param Closure[]    $closures
 			* Executes a query which returns a data value
 			* $datahandler is if you wanna pass the data in a special format
 			* $action is executed after you get the results
 			* Statements like INSERT
 			*/
-		  public function executeQuery ( String $query, String $database, \Closure $datahandler = null, \Closure $action = null, array $data = [])
+		  public function executeQuery ( String $query, String $database, Closure $datahandler = null, Closure $action = null, array $data = [], Closure ...$closures)
 		  {
 
-				DatabaseAPI::getInstance()->getServer()->getAsyncPool()->submitTask(new SelectorTask( $query, $this, $datahandler, $action, $database , $data) );
+				DatabaseAPI::getInstance()->getServer()->getAsyncPool()->submitTask(new SelectorTask( $query, $this, $datahandler, $action, $database , $data, $closures) );
 		  }
 
 
 		  /**
 			* @param String        $query
 			* @param String        $database
-			* @param \Closure|null $action
+			* @param Closure|null $action
 			* @param array         $extra_data
 			* For Database Actions like INSERT, UPDATE
 			*/
-		  public function execute ( String $query, String $database, ?\Closure $action, array $extra_data = [] )
+		  public function execute ( String $query, String $database, ?Closure $action, array $extra_data = [] )
 		  {
 
 				Server::getInstance()->getAsyncPool()->submitTask( new InsertionTask( $query, $this, $database, $action, $extra_data) );
